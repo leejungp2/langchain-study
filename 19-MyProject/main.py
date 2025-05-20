5b4d2f0be12f713b18b4ba260c9580f1c5750dae
jjung-lee <leejungp2@gmail.com>
2025-05-20 13:13:01 +0900

    0520 commit

--- 19-MyProject/main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("RAG 비법노트 프로젝트")
-st.markdown(
-    """
-[강의링크 바로가기](https://fastcampus.co.kr/data_online_teddy)
-"""
-)
---
+++ 2505-DARTProject/main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("기업 분석 프로젝트")
+st.markdown(
+    """
+[DART 전자공시시스템 바로가기](https://dart.fss.or.kr/main.do)
+"""
+)
